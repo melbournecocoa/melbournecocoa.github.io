d90920c565b936b7a5011cfc6cb023b17ad5b794
Jesse Collis <jesse@jcmultimedia.com.au>
2016-02-03 05:01:06 +1100

    quck pass of 'about' page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,13 +11,57 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <p>About Melbourne Cocoaheads</p>
+                    <h2>About Melbourne Cocoaheads</h2>
                     <ul>
-                        <li>Mailing List</li>
-                        <li>History</li>
-                        <li>Github</li>
-                        <li>Twitter</li>
+                        <li><a href="https://groups.google.com/group/cocoaheadsau">Australian Cocoaheads Google Group Mailing List</a></li>
+                        <li><a href="https://github.com/melbournecocoa/MelbourneCocoaheadsHistory">History</a></li>
+                        <li><a href="https://github.com/melbournecocoa">Github</a></li>
+                        <li><a href="https://www.twitter.com/melbournecocoa">Twitter - @meloburnecocoa</a></li>
+                        <li><a href="https://melbournecocoa.slack.com">Slack - @meloburnecocoa</a> - ask for an invite</li>
                     </ul>
+
+                    <h2>Meetings</h2>
+
+                    <p>Meetings are scheduled for the second Thursday of the month from February to December.
+                        A meeting might be moved forward or back a week depending on where it falls in relation to
+                        public holidays or WWDC, so make sure you keep tabs on the schedule by subscribing to Google
+                        Calendar, keeping an eye out on the
+                        <a href="https://groups.google.com/group/cocoaheadsau">Google Group</a> or following
+                        <a href="https://www.twitter.com/melbournecocoa">@melbournecocoa</a>.
+                    </p>
+
+                    <p>The meetings start at 6:30pm and go through to 9:30pm. They usually go a little something like this:</p>
+
+                    <ol>
+                        <li>Introduction, news & announcements (10 minutes). If you're looking to hire, there is a chance to let the group know.</li>
+                        <li>First presentations (40-60 minutes).</li>
+                        <li>Break (30-45 minutes). Beer, pizza and a great opportunity to meet people.</li>
+                        <li>Second presentations and Lightning Talks (20-45 minutes).</li>
+                        <li>Final wrap-up. We usually head to a local pub.</li>
+                    </ol>
+
+                    <p>A diverse range of people come to the meetings including hobbyist developers, designers,
+                        freelance contractors, commercial and indie developers. Whatever your background, the Cocoaheads
+                        community is a fantastic, supportive group who are all ears when it comes to discussing
+                        development for iOS and OSX.
+                    </p>
+
+                    <h2>Presentation Formats</h2>
+
+                    <p>Normally a full length presentation is around 20 to 25 minutes, and lightning talks or app demos
+                        come in at about 5 minutes.</p>
+
+                    <h2>Hack Nights</h2>
+
+                    <p>In addition the the formal monthly meetups we also have a monthly hack night on the last Tuesday
+                        of the month at the Little Creature's Dining Hall. Bring your laptop along and hack over some
+                        great food, beer and conversation.
+                    </p>
+
+                    <p>The hack nights are organised by Luke Tupper. Follow him on Twitter (<a href="https://www.twitter.com/tupps">@tupps</a>
+                        for news and updates on upcoming hack nights.
+                    </p>
+
                 </div>
             </div>
     </article>
